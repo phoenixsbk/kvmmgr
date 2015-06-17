c9756e92f37726456480ec025a009258f08fb544
jiamzhang <jiamzhang@ebay.com>
2015-06-17 15:47:50 +0800

    c

--- frontend/webadmin/modules/uiadmin/src/main/webapp/res/common/menutree.js
+++ frontend/webadmin/modules/uiadmin/src/main/webapp/res/common/menutree.js
@@ -1,7 +1,7 @@
-var MENU_ITEMS = ["datacentershref","clustershref","hostshref","networkshref","storagehref","diskshref",
-                  "vmshref","poolshref","templateshref","volumeshref","usershref","eventshref"];
-var MENU_LINKS = ["datacenters.html","clusters.html","hosts.html","networks.html","storage.html","disks.html",
-                  "vms.html","pools.html","templates.html","volumes.html","users.html","events.html"];
+var MENU_ITEMS = ["datacentershref","clustershref","hostshref","networkshref","storagehref",
+                  "vmshref","templateshref","volumeshref","usershref","eventshref"];
+var MENU_LINKS = ["datacenters.html","clusters.html","hosts.html","networks.html","storage.html",
+                  "vms.html","templates.html","volumes.html","users.html","events.html"];
 
 $(document).ready(function() {
 	$("#menucontainer").load("menutree.inc", function() {
