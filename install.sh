0c667e05be98eb411f2dba7e38cc24688643a7e7
jiamzhang <jiamzhang@ebay.com>
2015-10-11 23:38:52 +0800

    aaa

---
+++ install.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+yum install -y http://resources.ovirt.org/pub/yum-repo/ovirt-release35.rpm
+yum install -y *.rpm
+
+chmod 777 /usr/share/ovirt-engine/setup/bin/ovirt-engine-setup
+chmod 777 /usr/share/ovirt-engine/bin/java-home
+chmod 777 /usr/share/ovirt-engine/bin/*.sh
+chmod 777 /usr/share/ovirt-engine/dbscripts/*.sh
+chmod 777 /usr/share/ovirt-engine/services/ovirt-engine/*.py
+chmod 777 /usr/share/ovirt-engine/services/ovirt-engine-notifier/*.py
+chmod 777 /usr/share/ovirt-engine/services/ovirt-websocket-proxy/*.py
+chmod 777 /usr/share/ovirt-engine/services/ovirt-fence-kdump-listener/*.py
+chmod 777 /usr/share/ovirt-engine/setup/dbutils/*.sh
+
+engine-setup --offline
